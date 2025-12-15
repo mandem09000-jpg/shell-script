a50e9b28783361d18111e79573b79be765668d7b
mandem09000-jpg <mandem0900@gmail.com>
2025-12-15 11:11:06 +0530

    conversation between my self & mom

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "ismail :: assalamualaikum ammijan"
+echo "ammijan :: walaikumsalaam kisa hy maa"
+echo "ismail :: acha hu ammijan temey kisey hein"
+echo "ammijan :: my bhi achii huu"
