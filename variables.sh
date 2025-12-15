8ccd9a36e92c8e1d8b2ea45913c3ae9efc92bc69
mandem09000-jpg <mandem0900@gmail.com>
2025-12-15 11:38:44 +0530

    conversation between sister & dad

--- variables.sh
+++ variables.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+PERSON1=zakiya
+PERSON2=abbujan
 
-
-
-PERSON1 =zakiya
-PERSON2 =abbujan
-
-echo "$PERSON1 :: assalamualaikum "
+echo "$PERSON1 :: assalamualaikum"
 echo "$PERSON2 :: walaikumsalaam kisa hy maa"
 echo "$PERSON1 :: acha hu temey kisey hein"
 echo "$PERSON2 :: my bhi achii huu"
