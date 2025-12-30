cdc17cde864ce4952ab9c94283c9273c28910f24
mandem09000-jpg <mandem0900@gmail.com>
2025-12-30 10:45:02 +0530

    conversation

---
+++ 3.variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# no space before or after equal
+
+
+PERSON1=Zakiya
+PERSON2=Abbujan
+
+echo "$PERSON1 :: assalamualaikum $PERSON2"
+echo "$PERSON2 :: walaikumsalaam kisa hy $PERSON1"
+echo "$PERSON1 :: acha hu temey kisey hein $PERSON1"
+echo "$PERSON2 :: my bhi achii huu $PERSON1"
+ 
---
+++ 4.variables1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# no space before or after equal
+
+
+PERSON1=Zabiulla
+PERSON2=Ammijan
+
+echo "$PERSON1 :: assalamualaikum $PERSON2"
+echo "$PERSON2 :: walaikumsalaam kisa hy $PERSON1"
+echo "$PERSON1 :: acha hu temey kisey hein $PERSON2"
+echo "$PERSON2 :: my bhi achii huu $PERSON1"
+ 
