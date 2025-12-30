ddf51fe083bf28940f4636f51e3a05054e849521
mandem09000-jpg <mandem0900@gmail.com>
2025-12-30 09:56:23 +0530

    conversation between zabiulla & ammijan

--- variables1.sh
+++ variables1.sh
@@ -9,6 +9,6 @@ PERSON2=Ammijan
 
 echo "$PERSON1 :: assalamualaikum $PERSON2"
 echo "$PERSON2 :: walaikumsalaam kisa hy $PERSON1"
-echo "$PERSON1 :: acha hu temey kisey hein $PERSON1"
+echo "$PERSON1 :: acha hu temey kisey hein $PERSON2"
 echo "$PERSON2 :: my bhi achii huu $PERSON1"
  
