f21ae1d8e4e1d6058cb6ad95f7cea88f4fe9288d
mandem09000-jpg <mandem0900@gmail.com>
2025-12-30 10:40:10 +0530

    arguments passing from outside

--- 5.variables2.sh
+++ 5.variables2.sh
@@ -4,11 +4,11 @@
 # no space before or after equal
 
 
-PERSON1=Zakiya
-PERSON2=Abbujan
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1 :: assalamualaikum $PERSON2"
 echo "$PERSON2 :: walaikumsalaam kisa hy $PERSON1"
-echo "$PERSON1 :: acha hu temey kisey hein $PERSON1"
+echo "$PERSON1 :: acha hu temey kisey hein $PERSON2"
 echo "$PERSON2 :: my bhi achii huu $PERSON1"
  
--- conversation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-echo "ismail :: assalamualaikum ammijan"
-echo "ammijan :: walaikumsalaam kisa hy maa"
-echo "ismail :: acha hu ammijan temey kisey hein"
-echo "ammijan :: my bhi achii huu"
