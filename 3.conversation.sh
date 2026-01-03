fd4b6f12b5a30267e33ebca0d1443eea02bc2150
mandem09000-jpg <mandem0900@gmail.com>
2026-01-03 09:50:48 +0530

    giving arguments from outside

--- 3.conversation.sh
+++ 3.conversation.sh
@@ -4,9 +4,9 @@ PERSON1=$1
 PERSON2=$2
 
 
-echo "PERSON1 :: assalamualaikum PERSON2"
-echo "PERSON2 :: walaikumsalaam kisa hy maa"
-echo "PERSON1 :: acha hu PERSON2 temey kisey hein"
-echo "PERSON2 :: my bhi achii huu"
+echo "$PERSON1 :: assalamualaikum $PERSON2"
+echo "$PERSON2 :: walaikumsalaam kisa hy maa"
+echo "$PERSON1 :: acha hu $PERSON2 temey kisey hein"
+echo "$PERSON2 :: my bhi achii huu"
 
 
