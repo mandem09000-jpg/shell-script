842be395384158239f87ec5bd77fd589c3e88a8d
mandem09000-jpg <mandem0900@gmail.com>
2026-01-03 11:07:31 +0530

    condition statement

---
+++ 6.conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+NUMBER=10
+
+if (
+    the given number is less than 10
+    echo "print :$(NUMBER=true)"
+)
+else (
+    the given number is greater than 10
+    echo "print :$(NUMBER=false)"
+)
