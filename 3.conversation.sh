b3d3d3aeb749b39a9fbeb17b4447d4c4c9b35947
mandem09000-jpg <mandem0900@gmail.com>
2026-01-03 09:39:04 +0530

    passing arguments from outside

---
+++ 3.conversation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "PERSON1 :: assalamualaikum PERSON2"
+echo "PERSON2 :: walaikumsalaam kisa hy maa"
+echo "PERSON1 :: acha hu PERSON2 temey kisey hein"
+echo "PERSON2 :: my bhi achii huu"
+
+
